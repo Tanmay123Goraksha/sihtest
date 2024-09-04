eb0202208b95d46f7c39038e82892319bdce978d
Tanmay123Goraksha <tanmay32goraksha@gmail.com>
2024-09-04 18:02:00 +0530

    NEW

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv";
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = new pg.Client({
   user: process.env.DB_USER,
